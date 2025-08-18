b00ceb245c9153be74ae456a0117a79042452abd
offlinestudios <info@offlinestudios.ca>
2025-08-18 10:23:34 -0400

    Delete eezlegal-backend directory

--- eezlegal-backend/src/routes/files.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint, jsonify
-files_bp = Blueprint('files', __name__)
-@files_bp.route('/files/analyze', methods=['POST'])
-def analyze_file():
-    return jsonify({'success': True})
